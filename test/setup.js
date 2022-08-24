b707ff41c173600fbf26ae6595f14ac0d1be4a1d
steve-dickinson <steve.dickinson@defra.gov.uk>
2022-08-24 15:32:32 +0100

    Update jest timeout

--- test/setup.js
+++ test/setup.js
@@ -1,7 +1,7 @@
 beforeEach(async () => {
   // Set reference to server in order to close the server during teardown.
   const createServer = require('../app/server')
-  jest.useFakeTimers()
+  jest.setTimeout(15000)
   const server = await createServer()
   await server.initialize()
   global.__SERVER__ = server
