61aa53397636b3986efed3e2bf222947e7a40072
Steve Dickinson <32902665+steve-dickinson@users.noreply.github.com>
2022-10-06 07:02:04 +0100

    Add monitoring (#22)
    * Add monitoring

--- app/routes/verify-login.js
+++ app/routes/verify-login.js
@@ -1,5 +1,6 @@
 const Joi = require('joi')
 const { lookupToken, setAuthCookie } = require('../auth')
+const { sendMonitoringEvent } = require('../event')
 
 function isRequestInvalid (cachedEmail, email) {
   return !cachedEmail || email !== cachedEmail
@@ -15,9 +16,10 @@ module.exports = [{
         email: Joi.string().email(),
         token: Joi.string().uuid().required()
       }),
-      failAction: async (_, h, error) => {
+      failAction: async (request, h, error) => {
         console.error(error)
-        return h.view('auth/verify-login-failed').code(400).takeover()
+        sendMonitoringEvent(request.yar.id, error.details[0].message, '')
+        return h.view('verify-login-failed').code(400).takeover()
       }
     },
     handler: async (request, h) => {
@@ -25,7 +27,8 @@ module.exports = [{
 
       const { email: cachedEmail, redirectTo, userType } = await lookupToken(request, token)
       if (isRequestInvalid(cachedEmail, email)) {
-        return h.view('auth/verify-login-failed').code(400)
+        sendMonitoringEvent(request.yar.id, 'Invalid token', email)
+        return h.view('verify-login-failed').code(400)
       }
 
       setAuthCookie(request, email, userType)
