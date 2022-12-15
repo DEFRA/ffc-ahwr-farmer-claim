da263a71926d6a65a9d2b711ecf0807de67ae841
Shivanshu Patel <shivanshuit914@gmail.com>
2022-12-15 16:00:20 +0000

    feat: add unit tests

--- test/unit/app/api-requests/users.test.js
+++ test/unit/app/api-requests/users.test.js
@@ -1,19 +1,30 @@
 const { usersContainer, usersFile } = require('../../../../app/config').storageConfig
 
-describe('Get users', () => {
+const mockConfig = require('../../../../app/config')
+
+describe('Get users from blob storage', () => {
   let downloadBlobMock
   let getByEmail
+  let eligibilityApi
   const email = 'hit@email.com'
 
   beforeEach(() => {
     jest.resetAllMocks()
     jest.resetModules()
 
+    jest.mock('../../../../app/config', () => ({
+      ...mockConfig,
+      eligibilityApiEnabled: false
+    }))
+
     downloadBlobMock = require('../../../../app/lib/storage/download-blob')
     jest.mock('../../../../app/lib/storage/download-blob')
 
     const users = require('../../../../app/api-requests/users')
     getByEmail = users.getByEmail
+
+    eligibilityApi = require('../../../../app/api-requests/eligibility-service')
+    jest.mock('../../../../app/api-requests/eligibility-service')
   })
 
   test('makes request to download users blob', async () => {
@@ -49,6 +60,7 @@ describe('Get users', () => {
 
     const res = await getByEmail(email)
 
+    expect(eligibilityApi.getEligibleUserByEmail).toHaveBeenCalledTimes(0)
     expect(res).toEqual(JSON.parse(fileContent)[0])
   })
 
@@ -62,5 +74,55 @@ describe('Get users', () => {
 
     expect(res).toEqual(JSON.parse(fileContent)[0])
     expect(res.isTest).toEqual(JSON.parse(fileContent)[0].isTest)
+  })
+})
+
+describe('Get users from eligibility service', () => {
+  let downloadBlobMock
+  let getByEmail
+  const email = 'hit@email.com'
+
+  beforeEach(() => {
+    jest.resetAllMocks()
+    jest.resetModules()
+
+    jest.mock('../../../../app/config', () => ({
+      ...mockConfig,
+      eligibilityApiEnabled: true
+    }))
+
+    downloadBlobMock = require('../../../../app/lib/storage/download-blob')
+    jest.mock('../../../../app/lib/storage/download-blob')
+
+    eligibilityApi = require('../../../../app/api-requests/eligibility-service')
+    jest.mock('../../../../app/api-requests/eligibility-service')
+
+    const users = require('../../../../app/api-requests/users')
+    getByEmail = users.getByEmail
+  })
+
+  test('makes request to download users blob', async () => {
+    await getByEmail('email')
+
+    expect(downloadBlobMock).toHaveBeenCalledTimes(0)
+    expect(eligibilityApi.getEligibleUserByEmail).toHaveBeenCalledTimes(1)
+    expect(eligibilityApi.getEligibleUserByEmail).toHaveBeenCalledWith('email')
+  })
+
+  test('return null when email doesn\'t match any users', async () => {
+    eligibilityApi.getEligibleUserByEmail.mockResolvedValue(null)
+
+    const res = await getByEmail('miss@email.com')
+
+    expect(res).toEqual(null)
+  })
+
+  test('return user data when email is matched', async () => {
+    const response = `{ "email": "${email}" }`
+    eligibilityApi.getEligibleUserByEmail.mockResolvedValue(response)
+
+    const res = await getByEmail(email)
+
+    expect(res).toEqual(response)
   })
 })
