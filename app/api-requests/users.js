e7e5165309ea79bdafdecc31357169c442fd0940
Shivanshu Patel <shivanshuit914@gmail.com>
2022-12-15 15:26:45 +0000

    feat: add eligiblity service

--- app/api-requests/users.js
+++ app/api-requests/users.js
@@ -1,5 +1,7 @@
 const downloadBlob = require('../lib/storage/download-blob')
 const { usersContainer, usersFile } = require('../config').storageConfig
+const { eligibilityApiEnabled } = require ('../config')
+const { getEligibleUserByEmail } = require('./eligibility-service')
 
 async function getUsers () {
   const contents = await downloadBlob(usersContainer, usersFile) ?? '[]'
@@ -7,6 +9,9 @@ async function getUsers () {
 }
 
 async function getByEmail (email) {
+  if (eligibilityApiEnabled) {
+    return await getEligibleUserByEmail(email)
+  }
   return (await getUsers()).find(x => x.email.toLowerCase() === email.toLowerCase())
 }
 
