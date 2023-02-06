0d54c16b31a970feec43997bd1e9c58e43dce87a
liamwilson-kainos <liam.wilson@kainos.com>
2023-02-06 12:18:27 +0000

    Improved send magic link email coverage

--- test/unit/app/lib/email/send-magic-link-email.test.js
+++ test/unit/app/lib/email/send-magic-link-email.test.js
@@ -1,13 +1,8 @@
-const sendMagicLinkEmail = require('../../../../../app/lib/email/send-magic-link-email')
-const { templateIdFarmerClaimLogin } = require('../../../../../app/config').notifyConfig
 const { farmerClaim } = require('../../../../../app/constants/user-types')
-
-const getToken = require('../../../../../app/lib/auth/get-token')
-jest.mock('../../../../../app/lib/auth/get-token')
-
-const sendEmail = require('../../../../../app/lib/email/send-email')
-jest.mock('../../../../../app/lib/email/send-email')
 let cacheData = { }
+let sendMagicLinkEmail
+let getToken
+let sendEmail
 const requestGetMock = {
   server:
       {
@@ -25,21 +20,30 @@ const requestGetMock = {
       }
 }
 
-const config = require('../../../../../app/config')
-jest.mock('../../../../../app/config', () => ({
-  ...jest.requireActual('../../../../../app/config'),
-  serviceName: 'Annual health and welfare review of livestock',
-  serviceUri: 'http://localhost:3004',
-  selectYourBusiness: {
-    enabled: false
-  }
-}))
-const serviceUri = config.serviceUri
-
 describe('Send Magic Link test', () => {
   const email = 'test@unit-test.com'
   const sendEmailResponse = true
   const testToken = '644a2a30-7487-4e98-a908-b5ecd82d5225'
+  const templateIdFarmerClaimLogin = '6fb2d1f6-9be6-4bb6-a862-6c31386814ac'
+
+  beforeAll(() => {
+    jest.resetModules()
+    jest.mock('../../../../../app/config', () => ({
+      ...jest.requireActual('../../../../../app/config'),
+      serviceName: 'Annual health and welfare review of livestock',
+      serviceUri: 'http://localhost:3004',
+      selectYourBusiness: {
+        enabled: false
+      }
+    }))
+    sendMagicLinkEmail = require('../../../../../app/lib/email/send-magic-link-email')
+
+    getToken = require('../../../../../app/lib/auth/get-token')
+    jest.mock('../../../../../app/lib/auth/get-token')
+
+    sendEmail = require('../../../../../app/lib/email/send-email')
+    jest.mock('../../../../../app/lib/email/send-email')
+  })
 
   beforeEach(() => {
     cacheData = {}
@@ -59,8 +63,53 @@ describe('Send Magic Link test', () => {
     expect(cacheData[token]).toEqual({ email, redirectTo: 'visit-review', userType: farmerClaim })
     expect(sendEmail).toHaveBeenCalledTimes(1)
     expect(sendEmail).toHaveBeenCalledWith(templateIdFarmerClaimLogin, email, {
-      personalisation: { magiclink: `${serviceUri}/verify-login?token=${token}&email=${email}` },
+      personalisation: { magiclink: `http://localhost:3004/verify-login?token=${token}&email=${email}` },
       reference: token
+    })
+  })
+
+  describe('Send Magic Link test with select your business enabled', () => {
+    const email = 'test@unit-test.com'
+    const sendEmailResponse = true
+    const testToken = '644a2a30-7487-4e98-a908-b5ecd82d5225'
+    const templateIdFarmerClaimLogin = '6fb2d1f6-9be6-4bb6-a862-6c31386814ac'
+
+    beforeAll(() => {
+      jest.resetModules()
+      jest.mock('../../../../../app/config', () => ({
+        ...jest.requireActual('../../../../../app/config'),
+        serviceName: 'Annual health and welfare review of livestock',
+        serviceUri: 'http://localhost:3004',
+        selectYourBusiness: {
+          enabled: true
+        }
+      }))
+      sendMagicLinkEmail = require('../../../../../app/lib/email/send-magic-link-email')
+
+      getToken = require('../../../../../app/lib/auth/get-token')
+      jest.mock('../../../../../app/lib/auth/get-token')
+      sendEmail = require('../../../../../app/lib/email/send-email')
+      jest.mock('../../../../../app/lib/email/send-email')
+    })
+
+    beforeEach(() => {
+      cacheData = {}
+      jest.resetAllMocks()
+      sendEmail.mockResolvedValue(sendEmailResponse)
+    })
+
+    test('sends email for farmer claim', async () => {
+      const token = testToken
+      getToken.mockResolvedValue(token)
+      const response = await sendMagicLinkEmail.sendFarmerClaimLoginMagicLink(requestGetMock, email)
+      expect(response).toEqual(sendEmailResponse)
+      expect(cacheData[email]).toEqual([token])
+      expect(cacheData[token]).toEqual({ email, redirectTo: 'select-your-business', userType: farmerClaim })
+      expect(sendEmail).toHaveBeenCalledTimes(1)
+      expect(sendEmail).toHaveBeenCalledWith(templateIdFarmerClaimLogin, email, {
+        personalisation: { magiclink: `http://localhost:3004/verify-login?token=${token}&email=${email}` },
+        reference: token
+      })
     })
   })
 })
