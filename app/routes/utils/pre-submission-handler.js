4f5dd6b58772ff3f7fc6ded9e2e8f0b9fb7b478a
Marcin Mogiela <marcinmo@kainos.com>
2023-03-21 10:06:36 +0100

    FFC-161904: delete genererate new crumb from preSubmissionHandler

--- app/routes/utils/pre-submission-handler.js
+++ app/routes/utils/pre-submission-handler.js
@@ -1,22 +1,14 @@
 const Boom = require('@hapi/boom')
 const crumbCache = require('./crumb-cache')
 
-const generateNewCrumb = async (request, h) => {
-  delete request.plugins.crumb
-  request.server.plugins.crumb.generate(request, h)
-  console.log('New crumb created: %s', request.plugins.crumb)
-}
-
 const preSubmissionHandler = async (request, h) => {
   if (request.method === 'post') {
     const lookupCrumb = await crumbCache.lookupSubmissionCrumb(request)
     if (lookupCrumb?.crumb) {
       console.log('Duplicate crumb found: %s', request.plugins.crumb)
-      await generateNewCrumb(request, h)
       return Boom.forbidden('Duplicate submission')
     } else {
       await crumbCache.cacheSubmissionCrumb(request)
-      await generateNewCrumb(request, h)
       return h.continue
     }
   } else {
