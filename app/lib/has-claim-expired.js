91d55871bf3fbd9d4ec8465d0fe9b60fede72c8d
liamwilson-kainos <liam.wilson@kainos.com>
2023-05-24 14:00:55 +0100

    added unti test for has claim expired function

--- app/lib/has-claim-expired.js
+++ app/lib/has-claim-expired.js
@@ -2,11 +2,11 @@
 const config = require('../../app/config')
 
 function hasClaimExpired (application) {
-  console.log(`Checking if agreement with reference ${application.reference} and start date of ${application.createdAt} has exceeded claim deadline of ${config.claimExpiryTimeMonths} months.`)
-  let endDate = new Date(application.createdAt)
-  endDate = new Date(endDate.setMonth(endDate.getMonth() + config.claimExpiryTimeMonths))
-  const currentDate = new Date()
-  return currentDate > endDate
+  const startDate = new Date(application.createdAt)
+  const endDate = new Date(startDate)
+  endDate.setMonth(endDate.getMonth() + config.claimExpiryTimeMonths)
+  console.log(`Checking if agreement with reference ${application.reference}, start date of ${startDate} and end date of ${endDate} has exceeded claim deadline of ${config.claimExpiryTimeMonths} months.`)
+  return Date.now() > endDate
 }
 
 module.exports = {
