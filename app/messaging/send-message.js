f0fa2d2bc48f64df3f3630dcd05cb04e186b02f6
Marcin Mogiela <marcinmo@kainos.com>
2023-07-04 13:55:09 +0200

    optimise message sender/receiver

--- app/messaging/send-message.js
+++ app/messaging/send-message.js
@@ -1,11 +1,10 @@
-const { MessageSender } = require('ffc-messaging')
 const createMessage = require('./create-message')
+const { createMessageSender } = require('./create-message-sender')
 
 const sendMessage = async (body, type, config, options) => {
   const message = createMessage(body, type, options)
-  const sender = new MessageSender(config)
+  const sender = createMessageSender(config)
   await sender.sendMessage(message)
-  await sender.closeConnection()
 }
 
 module.exports = sendMessage
