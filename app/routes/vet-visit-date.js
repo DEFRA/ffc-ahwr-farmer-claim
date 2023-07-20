21e002fabe28eb323cc0b10a1efac349f2b7e8d6
Marcin Mogiela <marcinmo@kainos.com>
2023-07-20 10:31:10 +0200

    feature toggle

--- app/routes/vet-visit-date.js
+++ app/routes/vet-visit-date.js
@@ -26,23 +26,26 @@ module.exports = [{
       const dateOfTesting = session.getClaim(request, sessionKeys.farmerApplyData.dateOfTesting)
 
       return h.view(templatePath, {
+        dateOfTestingEnabled: config.dateOfTesting.enabled,
         items: createItemsFromDate(new Date(dateOfReview), false),
-        whenTestingWasCarriedOut: {
-          value: dateOfReview === dateOfTesting
-            ? 'whenTheVetVisitedTheFarmToCarryOutTheReview'
-            : 'onAnotherDate',
-          onAnotherDate: {
-            day: {
-              value: new Date(dateOfTesting).getDate()
-            },
-            month: {
-              value: new Date(dateOfTesting).getMonth() + 1
-            },
-            year: {
-              value: new Date(dateOfTesting).getFullYear()
+        whenTestingWasCarriedOut: config.dateOfTesting.enabled
+          ? {
+              value: dateOfReview === dateOfTesting
+                ? 'whenTheVetVisitedTheFarmToCarryOutTheReview'
+                : 'onAnotherDate',
+              onAnotherDate: {
+                day: {
+                  value: new Date(dateOfTesting).getDate()
+                },
+                month: {
+                  value: new Date(dateOfTesting).getMonth() + 1
+                },
+                year: {
+                  value: new Date(dateOfTesting).getFullYear()
+                }
+              }
             }
-          }
-        }
+          : {}
       })
     }
   }
@@ -51,61 +54,74 @@ module.exports = [{
   path,
   options: {
     validate: {
-      payload: Joi.object({
-        [labels.day]: Joi.number().min(1)
-          .when(labels.month, {
-            switch: [
-              { is: 2, then: Joi.number().max(28) },
-              { is: Joi.number().valid(4, 6, 9, 11), then: Joi.number().max(30), otherwise: Joi.number().max(31) }
-            ]
+      payload: config.dateOfTesting.enabled
+        ? Joi.object({
+            [labels.day]: Joi.number().min(1)
+              .when(labels.month, {
+                switch: [
+                  { is: 2, then: Joi.number().max(28) },
+                  { is: Joi.number().valid(4, 6, 9, 11), then: Joi.number().max(30), otherwise: Joi.number().max(31) }
+                ]
+              })
+              .required(),
+            [labels.month]: Joi.number().min(1).max(12).required(),
+            [labels.year]: Joi.number().min(2022).max(2024).required(),
+
+            whenTestingWasCarriedOut: Joi.string()
+              .valid('whenTheVetVisitedTheFarmToCarryOutTheReview', 'onAnotherDate')
+              .required(),
+
+            'on-another-date-day': Joi.number()
+              .when('whenTestingWasCarriedOut', {
+                switch: [
+                  {
+                    is: 'onAnotherDate',
+                    then: Joi
+                      .number()
+                      .min(1)
+                      .when('on-another-date-month', {
+                        switch: [
+                          { is: 2, then: Joi.number().max(28) },
+                          { is: Joi.number().valid(4, 6, 9, 11), then: Joi.number().max(30), otherwise: Joi.number().max(31) }
+                        ]
+                      })
+                      .required()
+                  },
+                  { is: 'whenTheVetVisitedTheFarmToCarryOutTheReview', then: Joi.allow('') }
+                ],
+                otherwise: Joi.allow('')
+              }),
+
+            'on-another-date-month': Joi.number()
+              .when('whenTestingWasCarriedOut', {
+                switch: [
+                  { is: 'onAnotherDate', then: Joi.number().min(1).max(12).required() },
+                  { is: 'whenTheVetVisitedTheFarmToCarryOutTheReview', then: Joi.allow('') }
+                ],
+                otherwise: Joi.allow('')
+              }),
+
+            'on-another-date-year': Joi.number()
+              .when('whenTestingWasCarriedOut', {
+                switch: [
+                  { is: 'onAnotherDate', then: Joi.number().min(2022).max(2024).required() },
+                  { is: 'whenTheVetVisitedTheFarmToCarryOutTheReview', then: Joi.allow('') }
+                ],
+                otherwise: Joi.allow('')
+              })
           })
-          .required(),
-        [labels.month]: Joi.number().min(1).max(12).required(),
-        [labels.year]: Joi.number().min(2022).max(2024).required(),
-
-        whenTestingWasCarriedOut: Joi.string()
-          .valid('whenTheVetVisitedTheFarmToCarryOutTheReview', 'onAnotherDate')
-          .required(),
-
-        'on-another-date-day': Joi.number()
-          .when('whenTestingWasCarriedOut', {
-            switch: [
-              {
-                is: 'onAnotherDate',
-                then: Joi
-                  .number()
-                  .min(1)
-                  .when('on-another-date-month', {
-                    switch: [
-                      { is: 2, then: Joi.number().max(28) },
-                      { is: Joi.number().valid(4, 6, 9, 11), then: Joi.number().max(30), otherwise: Joi.number().max(31) }
-                    ]
-                  })
-                  .required()
-              },
-              { is: 'whenTheVetVisitedTheFarmToCarryOutTheReview', then: Joi.allow('') }
-            ],
-            otherwise: Joi.allow('')
-          }),
-
-        'on-another-date-month': Joi.number()
-          .when('whenTestingWasCarriedOut', {
-            switch: [
-              { is: 'onAnotherDate', then: Joi.number().min(1).max(12).required() },
-              { is: 'whenTheVetVisitedTheFarmToCarryOutTheReview', then: Joi.allow('') }
-            ],
-            otherwise: Joi.allow('')
-          }),
-
-        'on-another-date-year': Joi.number()
-          .when('whenTestingWasCarriedOut', {
-            switch: [
-              { is: 'onAnotherDate', then: Joi.number().min(2022).max(2024).required() },
-              { is: 'whenTheVetVisitedTheFarmToCarryOutTheReview', then: Joi.allow('') }
-            ],
-            otherwise: Joi.allow('')
-          })
-      }),
+        : Joi.object({
+          [labels.day]: Joi.number().min(1)
+            .when(labels.month, {
+              switch: [
+                { is: 2, then: Joi.number().max(28) },
+                { is: Joi.number().valid(4, 6, 9, 11), then: Joi.number().max(30), otherwise: Joi.number().max(31) }
+              ]
+            })
+            .required(),
+          [labels.month]: Joi.number().min(1).max(12).required(),
+          [labels.year]: Joi.number().min(2022).max(2024).required()
+        }),
       failAction: async (request, h, error) => {
         const { createdAt } = session.getClaim(request)
         const dateInputErrors = getDateInputErrors(
@@ -134,32 +150,35 @@ module.exports = [{
         }
         return h
           .view(templatePath, {
+            dateOfTestingEnabled: config.dateOfTesting.enabled,
             ...request.payload,
             ...dateInputErrors,
             errorSummary,
-            whenTestingWasCarriedOut: {
-              value: request.payload.whenTestingWasCarriedOut,
-              errorMessage: error.details.find(e => e.context.label === 'whenTestingWasCarriedOut')
-                ? { text: 'Select if testing was carried out when the vet visited the farm or on another date' }
-                : undefined,
-              onAnotherDate: {
-                day: {
-                  value: request.payload['on-another-date-day'],
-                  error: error.details.find(e => e.context.label === 'on-another-date-day')
-                },
-                month: {
-                  value: request.payload['on-another-date-month'],
-                  error: error.details.find(e => e.context.label === 'on-another-date-month')
-                },
-                year: {
-                  value: request.payload['on-another-date-year'],
-                  error: error.details.find(e => e.context.label === 'on-another-date-year')
-                },
-                errorMessage: error.details.filter(e => e.context.label.startsWith('on-another-date')).length
-                  ? { text: 'Enter a date' }
-                  : undefined
-              }
-            }
+            whenTestingWasCarriedOut: config.dateOfTesting.enabled
+              ? {
+                  value: request.payload.whenTestingWasCarriedOut,
+                  errorMessage: error.details.find(e => e.context.label === 'whenTestingWasCarriedOut')
+                    ? { text: 'Select if testing was carried out when the vet visited the farm or on another date' }
+                    : undefined,
+                  onAnotherDate: {
+                    day: {
+                      value: request.payload['on-another-date-day'],
+                      error: error.details.find(e => e.context.label === 'on-another-date-day')
+                    },
+                    month: {
+                      value: request.payload['on-another-date-month'],
+                      error: error.details.find(e => e.context.label === 'on-another-date-month')
+                    },
+                    year: {
+                      value: request.payload['on-another-date-year'],
+                      error: error.details.find(e => e.context.label === 'on-another-date-year')
+                    },
+                    errorMessage: error.details.filter(e => e.context.label.startsWith('on-another-date')).length
+                      ? { text: 'Enter a date' }
+                      : undefined
+                  }
+                }
+              : {}
           })
           .code(400)
           .takeover()
@@ -184,23 +203,26 @@ module.exports = [{
           })
         }
         return h.view(templatePath, {
+          dateOfTestingEnabled: config.dateOfTesting.enabled,
           ...request.payload,
           ...dateInputErrors,
           errorSummary,
-          whenTestingWasCarriedOut: {
-            value: request.payload.whenTestingWasCarriedOut,
-            onAnotherDate: {
-              day: {
-                value: request.payload['on-another-date-day']
-              },
-              month: {
-                value: request.payload['on-another-date-month']
-              },
-              year: {
-                value: request.payload['on-another-date-year']
+          whenTestingWasCarriedOut: config.dateOfTesting.enabled
+            ? {
+                value: request.payload.whenTestingWasCarriedOut,
+                onAnotherDate: {
+                  day: {
+                    value: request.payload['on-another-date-day']
+                  },
+                  month: {
+                    value: request.payload['on-another-date-month']
+                  },
+                  year: {
+                    value: request.payload['on-another-date-year']
+                  }
+                }
               }
-            }
-          }
+            : {}
         }).code(400).takeover()
       }
       if (dateOfReview > endDate || dateOfReview < applicationDate) {
@@ -216,32 +238,37 @@ module.exports = [{
           })
         }
         return h.view(templatePath, {
+          dateOfTestingEnabled: config.dateOfTesting.enabled,
           ...request.payload,
           ...dateInputErrors,
           errorSummary,
-          whenTestingWasCarriedOut: {
-            value: request.payload.whenTestingWasCarriedOut,
-            onAnotherDate: {
-              day: {
-                value: request.payload['on-another-date-day']
-              },
-              month: {
-                value: request.payload['on-another-date-month']
-              },
-              year: {
-                value: request.payload['on-another-date-year']
+          whenTestingWasCarriedOut: config.dateOfTesting.enabled
+            ? {
+                value: request.payload.whenTestingWasCarriedOut,
+                onAnotherDate: {
+                  day: {
+                    value: request.payload['on-another-date-day']
+                  },
+                  month: {
+                    value: request.payload['on-another-date-month']
+                  },
+                  year: {
+                    value: request.payload['on-another-date-year']
+                  }
+                }
               }
-            }
-          }
+            : {}
         }).code(400).takeover()
       }
-      const dateOfTesting = request.payload.whenTestingWasCarriedOut === 'whenTheVetVisitedTheFarmToCarryOutTheReview'
-        ? dateOfReview
-        : new Date(
-          request.payload['on-another-date-year'],
-          request.payload['on-another-date-month'] - 1,
-          request.payload['on-another-date-day']
-        )
+      const dateOfTesting = config.dateOfTesting.enabled
+        ? request.payload.whenTestingWasCarriedOut === 'whenTheVetVisitedTheFarmToCarryOutTheReview'
+            ? dateOfReview
+            : new Date(
+              request.payload['on-another-date-year'],
+              request.payload['on-another-date-month'] - 1,
+              request.payload['on-another-date-day']
+            )
+        : dateOfReview
       session.setClaim(request, sessionKeys.farmerApplyData.visitDate, dateOfReview)
       session.setClaim(request, sessionKeys.farmerApplyData.dateOfTesting, dateOfTesting)
       return h.redirect('/claim/vet-name')
