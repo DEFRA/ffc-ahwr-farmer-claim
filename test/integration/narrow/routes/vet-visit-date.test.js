adb43f1ae9cb9463a03c9f3389d3ec0963b5b23e
Marcin Mogiela <marcinmo@kainos.com>
2023-07-21 16:01:32 +0200

    added a test

--- test/integration/narrow/routes/vet-visit-date.test.js
+++ test/integration/narrow/routes/vet-visit-date.test.js
@@ -368,6 +368,19 @@ describe('Vet, enter date of visit', () => {
           errorMessage: 'The date of testing must be in the past',
           errorHighlights: ['on-another-date-day', 'on-another-date-month', 'on-another-date-year'],
           applicationCreationDate: today
+        },
+        {
+          description: 'onAnotherDay - must not be before agreement acceptance date',
+          day: today.getDate(),
+          month: today.getMonth() + 1,
+          year: today.getFullYear(),
+          whenTestingWasCarriedOut: 'onAnotherDate',
+          onAnotherDateDay: yesterday.getDate(),
+          onAnotherDateMonth: yesterday.getMonth() + 1,
+          onAnotherDateYear: yesterday.getFullYear(),
+          errorMessage: `Date must be the same or after ${today.toLocaleString('en-GB', { day: 'numeric', month: 'long', year: 'numeric' })} when you accepted your agreement offer`,
+          errorHighlights: ['on-another-date-day', 'on-another-date-month', 'on-another-date-year'],
+          applicationCreationDate: today
         }
       ])('returns error ($errorMessage) when partial or invalid input is given - $description', async ({ day, month, year, whenTestingWasCarriedOut, onAnotherDateDay, onAnotherDateMonth, onAnotherDateYear, errorMessage, errorHighlights, applicationCreationDate }) => {
         session.getClaim.mockReturnValueOnce({ createdAt: applicationCreationDate })
