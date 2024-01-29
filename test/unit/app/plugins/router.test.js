d5c07e7b29e9b9200ad0d97f6caf76e76d271cec
Alex White <alex.a.white@capgemini.com>
2024-01-29 16:03:21 +0000

    Routes test

---
+++ test/unit/app/plugins/router.test.js
@@ -0,0 +1,86 @@
+describe('routes plugin test', () => {
+  jest.mock('../../../../app/config', () => ({
+    ...jest.requireActual('../../../../app/config'),
+    endemics: {
+      enabled: false
+    }
+  }))
+
+  beforeEach(() => {
+    jest.resetModules()
+    jest.clearAllMocks()
+  })
+
+  test('routes included', async () => {
+    const createServer = require('../../../../app/server')
+    const server = await createServer()
+    const routePaths = []
+    server.table().forEach((element) => {
+      routePaths.push(element.path)
+    })
+    expect(routePaths).toEqual(['/claim',
+      '/healthy',
+      '/healthz',
+      '/claim/animals-tested',
+      '/claim/check-answers',
+      '/claim/cookies',
+      '/claim/details-incorrect',
+      '/claim/number-of-animals-ineligible',
+      '/claim/signin-oidc',
+      '/claim/submit-claim',
+      '/claim/urn-result',
+      '/claim/vet-name',
+      '/claim/vet-rcvs',
+      '/claim/vet-visit-date',
+      '/claim/visit-review',
+      '/claim/assets/{path*}',
+      '/claim/animals-tested',
+      '/claim/cookies',
+      '/claim/submit-claim',
+      '/claim/urn-result',
+      '/claim/vet-name',
+      '/claim/vet-rcvs',
+      '/claim/vet-visit-date',
+      '/claim/visit-review'])
+  })
+
+  test('routes included - endemics enabled', async () => {
+    jest.mock('../../../../app/config', () => ({
+      ...jest.requireActual('../../../../app/config'),
+      endemics: {
+        enabled: true
+      }
+    }))
+
+    const createServer = require('../../../../app/server')
+    const server = await createServer()
+    const routePaths = []
+    server.table().forEach((element) => {
+      routePaths.push(element.path)
+    })
+    expect(routePaths).toEqual(['/claim',
+      '/healthy',
+      '/healthz',
+      '/claim/animals-tested',
+      '/claim/check-answers',
+      '/claim/cookies',
+      '/claim/details-incorrect',
+      '/claim/number-of-animals-ineligible',
+      '/claim/signin-oidc',
+      '/claim/submit-claim',
+      '/claim/urn-result',
+      '/claim/vet-name',
+      '/claim/vet-rcvs',
+      '/claim/vet-visit-date',
+      '/claim/visit-review',
+      '/claim/assets/{path*}',
+      '/claim/animals-tested',
+      '/claim/cookies',
+      '/claim/submit-claim',
+      '/claim/urn-result',
+      '/claim/vet-name',
+      '/claim/vet-rcvs',
+      '/claim/vet-visit-date',
+      '/claim/visit-review'])
+  })
+})
