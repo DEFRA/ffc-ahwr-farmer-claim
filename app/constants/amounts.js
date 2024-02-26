1784d9b85a63578fb4f3ae91c10b4db7f50be9d5
Afshin-Karamifar-Capgemini <afshin.karamifar@capgemini.com>
2024-02-26 19:16:14 +0000

    Ticket 301811 - Minimum number of animal tested

--- app/constants/amounts.js
+++ app/constants/amounts.js
@@ -1,3 +1,5 @@
+const { livestockTypes: { beef, dairy, pigs, sheep }, claimType: { review, endemics } } = require('./claim')
+
 const amounts = {
   beef: 522,
   dairy: 372,
@@ -6,10 +8,23 @@ const amounts = {
 }
 
 const thresholds = {
-  minimumNumberFluidOralSamples: 5,
-  minimumNumberOFBeefTested: 5,
-  minimumNumberOFPigsTested: 30,
-  minimumNumberOFSheepTested: 10
+  [beef]: {
+    [review]: 5,
+    [endemics]: 1
+  },
+  [dairy]: {
+    [review]: 5,
+    [endemics]: 1
+  },
+  [pigs]: {
+    [review]: 30,
+    [endemics]: 30
+  },
+  [sheep]: {
+    [review]: 10,
+    [endemics]: 10
+  },
+  minimumNumberFluidOralSamples: 5
 }
 
 module.exports = { amounts, thresholds }
