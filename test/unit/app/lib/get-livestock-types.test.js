da505fa22e1b28037528e020ee684b9850586ee0
JulieLev <JulieLev@users.noreply.github.com>
2024-04-18 01:18:21 +0100

    added getLivestockTypes test

---
+++ test/unit/app/lib/get-livestock-types.test.js
@@ -0,0 +1,44 @@
+const { getLivestockTypes } = require('../../../../app/lib/get-livestock-types')
+
+describe('getLivestockTypes', () => {
+  let typeOfLivestock
+  test('returns correct value for beef', () => {
+    typeOfLivestock = 'beef'
+    const { isBeef, isDairy, isPigs, isSheep } = getLivestockTypes(typeOfLivestock)
+
+    expect(isBeef).toBe(true)
+    expect(isDairy).toBe(false)
+    expect(isPigs).toBe(false)
+    expect(isSheep).toBe(false)
+  })
+
+  test('returns correct value for dairy', () => {
+    typeOfLivestock = 'dairy'
+    const { isBeef, isDairy, isPigs, isSheep } = getLivestockTypes(typeOfLivestock)
+
+    expect(isBeef).toBe(false)
+    expect(isDairy).toBe(true)
+    expect(isPigs).toBe(false)
+    expect(isSheep).toBe(false)
+  })
+
+  test('returns correct value for pigs', () => {
+    typeOfLivestock = 'pigs'
+    const { isBeef, isDairy, isPigs, isSheep } = getLivestockTypes(typeOfLivestock)
+
+    expect(isBeef).toBe(false)
+    expect(isDairy).toBe(false)
+    expect(isPigs).toBe(true)
+    expect(isSheep).toBe(false)
+  })
+
+  test('returns correct value for sheep', () => {
+    typeOfLivestock = 'sheep'
+    const { isBeef, isDairy, isPigs, isSheep } = getLivestockTypes(typeOfLivestock)
+
+    expect(isBeef).toBe(false)
+    expect(isDairy).toBe(false)
+    expect(isPigs).toBe(false)
+    expect(isSheep).toBe(true)
+  })
+})
