fce4bbd0ff9110b3d24b0636f33f5913eeab01df
elamin.fadlalla <elamin.fadlalla@capgemini.com>
2024-05-12 19:19:13 +0100

    change content

--- app/constants/sheep-test-types.js
+++ app/constants/sheep-test-types.js
@@ -15,6 +15,7 @@ const sheepTestTypes = {
     { value: 'cla', text: 'Caseous Lymphadenitis (CLA)' },
     { value: 'opa', text: 'Ovine Pulmonary Adenocarcinoma (OPA)' },
     { value: 'traceElements', text: 'Trace elements' },
+    { value: 'liverFluke', text: 'Liver fluke' },
     { value: 'haemonchosis', text: 'Haemonchosis' },
     { value: 'eweNutritionStatus', text: 'Ewe nutrition status' },
     { value: 'mastitis', text: 'Mastitis' },
@@ -35,7 +36,6 @@ const sheepTestTypes = {
     { value: 'other', text: 'Other' }
   ],
   improvedLambPerformance: [
-    { value: 'lameness', text: 'Lameness' },
     { value: 'bd', text: 'Border disease (BD)' },
     { value: 'traceElements', text: 'Trace elements' },
     { value: 'liverFluke', text: 'Liver fluke' },
