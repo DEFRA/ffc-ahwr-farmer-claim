75d5d12ce12600be8b8fe34fb32133cf1ccbebdb
Alex White <alex.a.white@capgemini.com>
2024-05-13 15:35:45 +0100

    Fix link and padding

--- app/routes/endemics/which-type-of-review.js
+++ app/routes/endemics/which-type-of-review.js
@@ -77,7 +77,7 @@ module.exports = [
           return h
             .view(endemicsWhichTypeOfReviewDairyFollowUpException, {
               backLink: pageUrl,
-              manageYourClaims: claimDashboard,
+              claimDashboard,
               ruralPaymentsAgency
             })
             .code(400)
