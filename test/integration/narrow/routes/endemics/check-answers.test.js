7ba8a6d11f6961464f735a46c8cc50221ed429f7
takano <tosin.akano@capgemini.com>
2024-05-30 11:09:33 +0100

    restart test

--- test/integration/narrow/routes/endemics/check-answers.test.js
+++ test/integration/narrow/routes/endemics/check-answers.test.js
@@ -132,6 +132,7 @@ describe('Check answers test', () => {
         const rowContents = getRowContents($)
         const rowActionTexts = getRowActionTexts($)
         const rowLinks = getRowLinks($)
+        
 
         expect(rowKeys).toEqual(expectedReviewDairy.rowKeys)
         expect(rowContents).toEqual(expectedReviewDairy.rowContents)
