9cec2f4876a63e5a6b8d1991455de0575f3e3fed
govTechSatish <satish.chatap@capgemini.com>
2024-06-08 18:01:42 +0100

    removed test word from ship test result page

--- test/integration/narrow/routes/endemics/sheep-test-results.test.js
+++ test/integration/narrow/routes/endemics/sheep-test-results.test.js
@@ -88,7 +88,7 @@ describe('Sheep test result tests', () => {
       const $ = cheerio.load(res.payload)
 
       expect(res.statusCode).toBe(400)
-      expect($('h1').text()).toMatch('What was the Flystrike test result?')
+      expect($('h1').text()).toMatch('What was the Flystrike result?')
       expect($('#testResult-error').text()).toMatch('Select a result')
       expect($('.govuk-back-link').attr('href')).toContain('/claim/endemics/sheep-tests')
     })
