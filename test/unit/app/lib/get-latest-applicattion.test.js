e70fa400ff8a79cc7fa6b22c5c479a367b815512
elamin.fadlalla <elamin.fadlalla@capgemini.com>
2024-06-20 15:26:36 +0100

    refactor getLatestApplicationForSbi

---
+++ test/unit/app/lib/get-latest-applicattion.test.js
@@ -0,0 +1,25 @@
+const { getLatestApplication } = require('../../../../app/lib/get-latest-application')
+
+describe('getLatestApplication', () => {
+  test('returns the latest application', () => {
+    const applications = [
+      { createdAt: '2022-01-01T00:00:00.000Z' },
+      { createdAt: '2023-01-01T00:00:00.000Z' },
+      { createdAt: '2022-06-01T00:00:00.000Z' }
+    ]
+    const expected = { createdAt: '2023-01-01T00:00:00.000Z' }
+    expect(getLatestApplication(applications)).toEqual(expected)
+  })
+
+  test('returns an empty object if no applications', () => {
+    const applications = []
+    const expected = {}
+    expect(getLatestApplication(applications)).toEqual(expected)
+  })
+
+  test('returns the only application if only one', () => {
+    const applications = [{ createdAt: '2022-01-01T00:00:00.000Z' }]
+    const expected = { createdAt: '2022-01-01T00:00:00.000Z' }
+    expect(getLatestApplication(applications)).toEqual(expected)
+  })
+})
