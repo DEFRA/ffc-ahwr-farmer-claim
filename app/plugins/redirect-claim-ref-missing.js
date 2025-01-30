ef9fe0ca3c07326558e52b49bde0ed4e83a7a6ad
rossjarvisesynergy <ross.jarvis@esynergy.co.uk>
2025-01-30 15:39:07 +0000

    removed log

--- app/plugins/redirect-claim-ref-missing.js
+++ app/plugins/redirect-claim-ref-missing.js
@@ -6,11 +6,6 @@ module.exports = {
     name: 'redirect-claim-ref-missing',
     register: (server, _) => {
       server.ext('onPreHandler', (request, h) => {
-        console.log({
-          method: request.method,
-          path: request.path,
-          request
-        })
         if (request.method === 'get' && request.path.includes('/claim/endemics/') && !request.path.includes('dev-sign-in') && !request.path.includes('assets')) {
           const claim = session.getEndemicsClaim(request)
           if (!claim?.reference) {
