8c38df7e844b46cab77b102e9d161eb46b3185c1
Rob Catton <robmcatton@gmail.com>
2025-05-06 14:59:35 +0100

    Ensuring non MH test files have MH turned OFF

--- test/integration/narrow/routes/endemics/date-of-testing.test.js
+++ test/integration/narrow/routes/endemics/date-of-testing.test.js
@@ -1,6 +1,6 @@
 import cheerio from 'cheerio'
 import { createServer } from '../../../../../app/server.js'
-import { setEndemicsAndOptionalPIHunt } from '../../../../mocks/config.js'
+import { setEndemicsAndOptionalPIHunt, setMultiHerds } from '../../../../mocks/config.js'
 import { getEndemicsClaim } from '../../../../../app/session/index.js'
 import expectPhaseBanner from 'assert'
 import { getReviewType } from '../../../../../app/lib/get-review-type.js'
@@ -50,6 +50,7 @@ describe('Date of testing when Optional PI Hunt is OFF', () => {
   let server
 
   beforeAll(async () => {
+    setMultiHerds(false)
     getEndemicsClaim.mockImplementation(() => { return { latestVetVisitApplication, latestEndemicsApplication: { createdAt: new Date('2022-01-01') }, reference: 'TEMP-6GSE-PIR8' } })
     setEndemicsAndOptionalPIHunt({ endemicsEnabled: true, optionalPIHuntEnabled: false })
     server = await createServer()
@@ -399,6 +400,7 @@ describe('Date of testing when Optional PI Hunt is ON', () => {
   let server
 
   beforeAll(async () => {
+    setMultiHerds(false)
     server = await createServer()
     await server.initialize()
     setEndemicsAndOptionalPIHunt({ endemicsEnabled: true, optionalPIHuntEnabled: true })
