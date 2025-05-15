1b023b0d16776f22601aef2ceab96aaabf09c063
rossjarvisesynergy <ross.jarvis@esynergy.co.uk>
2025-05-15 09:42:43 +0100

    removed spacing

--- app/routes/endemics/date-of-visit-mh.js
+++ app/routes/endemics/date-of-visit-mh.js
@@ -304,8 +304,8 @@ const postHandler = {
         if (herds.length) {
           return h.redirect(`${config.urlPrefix}/${endemicsSelectTheHerd}`)
         }
+        
         setEndemicsClaim(request, herdIdKey, getTempHerdId(request, herdId))
-
         if (!herdVersion) {
           setEndemicsClaim(request, herdVersionKey, 1)
         }
