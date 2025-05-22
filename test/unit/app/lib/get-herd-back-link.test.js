c3d4b4124c86a8e5bd6681ed6050b35c9d94ad9e
rossjarvisesynergy <ross.jarvis@esynergy.co.uk>
2025-05-22 15:22:02 +0100

    updated test

--- test/unit/app/lib/get-herd-back-link.test.js
+++ test/unit/app/lib/get-herd-back-link.test.js
@@ -1,32 +1,24 @@
 import * as contextHelper from '../../../../app/lib/context-helper.js'
-import { config } from '../../../../app/config/index.js'
 import { getHerdBackLink } from '../../../../app/lib/get-herd-back-link'
-import links from '../../../../app/config/routes.js'
 
 jest.mock('../../../../app/lib/context-helper.js', () => ({
   skipSameHerdPage: jest.fn()
 }))
 
 describe('getHerdBackLink', () => {
-  const typeOfLivestock = 'cattle'
-  const previousClaims = [{ claimId: 1 }]
-
-  const { urlPrefix } = config
-  const { endemicsCheckHerdDetails, endemicsSameHerd } = links
-
   it('should return CheckHerdDetails link if skipSameHerdPage returns true', () => {
     contextHelper.skipSameHerdPage.mockReturnValue(true)
 
-    const result = getHerdBackLink(typeOfLivestock, previousClaims)
+    const result = getHerdBackLink('cattle', [{ claimId: 1 }])
 
-    expect(result).toBe(`${urlPrefix}/${endemicsCheckHerdDetails}`)
+    expect(result).toBe('/claim/endemics/check-herd-details')
   })
 
   it('should return SameHerd link if skipSameHerdPage returns false', () => {
     contextHelper.skipSameHerdPage.mockReturnValue(false)
 
-    const result = getHerdBackLink(typeOfLivestock, previousClaims)
+    const result = getHerdBackLink('cattle', [{ claimId: 1 }])
 
-    expect(result).toBe(`${urlPrefix}/${endemicsSameHerd}`)
+    expect(result).toBe('/claim/endemics/same-herd')
   })
 })
