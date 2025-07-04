bc18f9a7e5df786be88251d6e6a08851306d1f6d
Rob Catton <robmcatton@gmail.com>
2025-07-04 15:40:56 +0100

    Added additional test, and added some logging to the number-of-samples-tested API handler

--- test/integration/narrow/routes/signin-oidc.test.js
+++ test/integration/narrow/routes/signin-oidc.test.js
@@ -1,5 +1,7 @@
 import { createServer } from '../../.././../app/server.js'
 import { authConfig } from '../../../../app/config/auth.js'
+import HttpStatus from 'http-status-codes'
+import cheerio from 'cheerio'
 
 describe('DefraID redirection test', () => {
   let server
@@ -42,8 +44,30 @@ describe('DefraID redirection test', () => {
 
       const res = await server.inject(options)
 
-      expect(res.statusCode).toBe(302)
+      expect(res.statusCode).toBe(HttpStatus.MOVED_TEMPORARILY)
       expect(res.headers.location).toEqual(`${authConfig.defraId.dashboardRedirectUri}?code=${code}&state=${state}`)
+    })
+
+    test('returns 302 and redirected to login failed page when an error is thrown', async () => {
+      const code = '432432'
+      const state = '83d2b160-74ce-4356-9709-3f8da7868e35'
+      const baseUrl = `${url}?code=${code}&state=${state}`
+      const options = {
+        method: 'GET',
+        url: baseUrl
+      }
+
+      global.URLSearchParams = function () {
+        throw new Error('forced test error')
+      }
+
+      const res = await server.inject(options)
+
+      expect(res.statusCode).toBe(HttpStatus.BAD_REQUEST)
+
+      const $ = cheerio.load(res.payload)
+      expect($('h1.govuk-heading-l').text()).toEqual('Login failed')
+      expect($('title').text()).toContain('Login failed')
     })
   })
 })
