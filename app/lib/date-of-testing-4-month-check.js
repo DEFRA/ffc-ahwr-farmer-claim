f676588ef6394669751a6f5c2c96aee1a90292f6
rossjarvisesynergy <ross.jarvis@esynergy.co.uk>
2025-07-22 11:48:58 +0100

    fixed sonar issues main

--- app/lib/date-of-testing-4-month-check.js
+++ app/lib/date-of-testing-4-month-check.js
@@ -1,8 +1,10 @@
+const AFTER_HOURS = 23; const AFTER_MINS = 59; const AFTER_SECONDS = 59; const AFTER_MILLIS = 999
+
 export const isWithin4MonthsBeforeOrAfterDateOfVisit = (dateOfVisit, dateOfTesting) => {
   const visitDate = new Date(dateOfVisit)
   const testingDate = new Date(dateOfTesting)
 
-  if (isNaN(visitDate) || isNaN(testingDate)) return false
+  if (isNaN(visitDate) || isNaN(testingDate)) { return false }
 
   const fourMonthsBefore = new Date(visitDate)
   fourMonthsBefore.setMonth(visitDate.getMonth() - 4)
@@ -10,7 +12,7 @@ export const isWithin4MonthsBeforeOrAfterDateOfVisit = (dateOfVisit, dateOfTesti
 
   const fourMonthsAfter = new Date(visitDate)
   fourMonthsAfter.setMonth(visitDate.getMonth() + 4)
-  fourMonthsAfter.setHours(23, 59, 59, 999)
+  fourMonthsAfter.setHours(AFTER_HOURS, AFTER_MINS, AFTER_SECONDS, AFTER_MILLIS)
 
   return testingDate >= fourMonthsBefore && testingDate <= fourMonthsAfter
 }
